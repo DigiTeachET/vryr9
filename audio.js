2d5dedeec6f02251de2692c2605fb092b40c5806
ElizabethTrudeau <108437287+DigiTeachET@users.noreply.github.com>
2025-03-13 08:02:55 +1300

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,18 +1,27 @@
 AFRAME.registerComponent('songplayer1', {
     init: function () {
-    let audiosource = document.querySelector('#track1');
-    let musicplay = () => {
-    audiosource.components.sound.playSound()
+        let audiosource = document.querySelector('#track1');
+        this.el.addEventListener('click', function () {
+            console.log("Play button clicked");
+            if (audiosource && audiosource.components.sound) {
+                audiosource.components.sound.playSound();
+            } else {
+                console.error("Sound component not found on track1");
+            }
+        });
     }
-    this.el.addEventListener('click', musicplay);
-    }});
-    
-    AFRAME.registerComponent('songstopper1', {
+});
+
+AFRAME.registerComponent('songstopper1', {
     init: function () {
-    let audiosource = document.querySelector('#track1');
-    let musicstop = () => {
-    audiosource.components.sound.stopSound()
+        let audiosource = document.querySelector('#track1');
+        this.el.addEventListener('click', function () {
+            console.log("Stop button clicked");
+            if (audiosource && audiosource.components.sound) {
+                audiosource.components.sound.stopSound();
+            } else {
+                console.error("Sound component not found on track1");
+            }
+        });
     }
-    this.el.addEventListener('click', musicstop);
-    }});
-    
+});
